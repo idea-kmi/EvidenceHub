71ada3fc24fb49639be5e492e0bd02c130ade47b
Michelle S Bachler <michelle.bachler@open.ac.uk>
2024-10-11 13:44:33 +0100

    Update setup_template.php

--- setup_template.php
+++ setup_template.php
@@ -31,147 +31,150 @@ if (!function_exists('is_countable')) {
 }
 
 /** SETUP THE FILE LOCATION MANAGER **/
-	unset($HUB_FLM);
-	require_once("core/filelocationmanager.class.php");
-	// instantiate the file location manager
-	if (isset($CFG->uitheme)) {
-		$HUB_FLM = new FileLocationManager($CFG->uitheme);
-	} else {
-		$HUB_FLM = new FileLocationManager();
-	}
-	global $HUB_FLM;
+unset($HUB_FLM);
+require_once("core/filelocationmanager.class.php");
+// instantiate the file location manager
+if (isset($CFG->uitheme)) {
+	$HUB_FLM = new FileLocationManager($CFG->uitheme);
+} else {
+	$HUB_FLM = new FileLocationManager();
+}
+global $HUB_FLM;
 
 
 /** SETUP STATIC CONFIG VARIABLES **/
 
-	$CFG->VERSION = '2.0';
+$CFG->VERSION = '2.0';
 
-	$CFG->HAS_SOLUTION = in_array('Solution', $CFG->BASE_TYPES);
-	$CFG->HAS_CLAIM = in_array('Claim', $CFG->BASE_TYPES);
-	$CFG->HAS_CHALLENGE = in_array('Challenge', $CFG->BASE_TYPES);
-	$CFG->HAS_OPEN_COMMENTS = in_array('Idea', $CFG->BASE_TYPES);
+$CFG->HAS_SOLUTION = in_array('Solution', $CFG->BASE_TYPES);
+$CFG->HAS_CLAIM = in_array('Claim', $CFG->BASE_TYPES);
+$CFG->HAS_CHALLENGE = in_array('Challenge', $CFG->BASE_TYPES);
+$CFG->HAS_OPEN_COMMENTS = in_array('Idea', $CFG->BASE_TYPES);
 
-	// historical when we had different Comment types. Could be phased out of the code now
-	$CFG->COMMENT_TYPES = array("Comment");
+// historical when we had different Comment types. Could be phased out of the code now
+$CFG->COMMENT_TYPES = array("Comment");
 
-	$CFG->actionAdd = "add";
-	$CFG->actionEdit = "edit";
-	$CFG->actionDelete = "delete";
+$CFG->actionAdd = "add";
+$CFG->actionEdit = "edit";
+$CFG->actionDelete = "delete";
 
-	/**
-	 * This is a legacy item. This is still required by the delete role code!
-	 * MB:look at removing
-	 */
-	$CFG->DEFAULT_NODE_TYPE = "Issue"; // This is still required by delete role!
+/**
+ * This is a legacy item. This is still required by the delete role code!
+ * MB:look at removing
+ */
+$CFG->DEFAULT_NODE_TYPE = "Issue"; // This is still required by delete role!
 
-	$CFG->AUTH_TYPE_EVHUB = "evhub";
+$CFG->AUTH_TYPE_EVHUB = "evhub";
 
-	$CFG->STATUS_ACTIVE = 0;
-	$CFG->STATUS_SPAM = 1;
+$CFG->STATUS_ACTIVE = 0;
+$CFG->STATUS_SPAM = 1;
 
-	$CFG->USER_STATUS_ACTIVE = 0;
-	$CFG->USER_STATUS_REPORTED = 1;
-	$CFG->USER_STATUS_UNVALIDATED = 2;
-	$CFG->USER_STATUS_UNAUTHORIZED = 3;
-	$CFG->USER_STATUS_SUSPENDED = 4;
+$CFG->USER_STATUS_ACTIVE = 0;
+$CFG->USER_STATUS_REPORTED = 1;
+$CFG->USER_STATUS_UNVALIDATED = 2;
+$CFG->USER_STATUS_UNAUTHORIZED = 3;
+$CFG->USER_STATUS_SUSPENDED = 4;
 
-	$CFG->GLOBAL_CONTEXT = "global";
-	$CFG->USER_CONTEXT = "user";
-	$CFG->NODE_CONTEXT = "node";
+$CFG->GLOBAL_CONTEXT = "global";
+$CFG->USER_CONTEXT = "user";
+$CFG->NODE_CONTEXT = "node";
 
-	$CFG->LINK_PRO_SOLUTION = 'supports';
-	$CFG->LINK_CON_SOLUTION = 'challenges';
+$CFG->LINK_PRO_SOLUTION = 'supports';
+$CFG->LINK_CON_SOLUTION = 'challenges';
 
-	$CFG->IMAGE_MAX_FILESIZE = 1000000;
-	$CFG->IMAGE_MAX_HEIGHT = 100;
-	$CFG->IMAGE_WIDTH = 60;
-	$CFG->IMAGE_HEIGHT = 90;
-	$CFG->IMAGE_THUMB_WIDTH = 30;
-	$CFG->ICON_WIDTH = 32;
-	$CFG->DEFAULT_USER_PHOTO= 'profile.png';
-	$CFG->DEFAULT_GROUP_PHOTO= 'groupprofile.png';
+$CFG->IMAGE_MAX_FILESIZE = 1000000;
+$CFG->IMAGE_MAX_HEIGHT = 100;
+$CFG->IMAGE_WIDTH = 60;
+$CFG->IMAGE_HEIGHT = 90;
+$CFG->IMAGE_THUMB_WIDTH = 30;
+$CFG->ICON_WIDTH = 32;
+$CFG->DEFAULT_USER_PHOTO= 'profile.png';
+$CFG->DEFAULT_GROUP_PHOTO= 'groupprofile.png';
 
-	// For calling the geo api to get location for addresses
-	// NOT SURE THIS IS USED ANYMORE?
-	$CFG->GOOLGE_SERVER_SIDE_API_KEY = "<***Add your google api key***>";
+// For calling the geo api to get location for addresses
+// NOT SURE THIS IS USED ANYMORE?
+$CFG->GOOLGE_SERVER_SIDE_API_KEY = "<***Add your google api key***>";
 
-	/**
-	 * The file paths for the node type icons used.
-	 * These are currently only referenced in ui/datamodel.js.php
-	 */
-	$CFG->challengeicon = $HUB_FLM->getImagePath("nodetypes/Default/challenge.png");
-	$CFG->issueicon = $HUB_FLM->getImagePath("nodetypes/Default/issue.png");
-	$CFG->claimicon = $HUB_FLM->getImagePath("nodetypes/Default/claim.png");
-	$CFG->solutionicon = $HUB_FLM->getImagePath("nodetypes/Default/solution.png");
-	$CFG->evidenceicon = $HUB_FLM->getImagePath("nodetypes/Default/litertaure-alaysis.png");
-	$CFG->resourceicon = $HUB_FLM->getImagePath("nodetypes/Default/reference-32x32.png");
-	$CFG->orgicon = $HUB_FLM->getImagePath("nodetypes/Default/organization.png");
-	$CFG->projecticon = $HUB_FLM->getImagePath("nodetypes/Default/project.png");
-	$CFG->proicon = $HUB_FLM->getImagePath("nodetypes/Default/plus-32x32.png");
-	$CFG->conicon = $HUB_FLM->getImagePath("nodetypes/Default/minus-32x32.png");
-	$CFG->themeicon = $HUB_FLM->getImagePath("nodetypes/Default/theme.png");
+/**
+ * The file paths for the node type icons used.
+ * These are currently only referenced in ui/datamodel.js.php
+ */
+$CFG->challengeicon = $HUB_FLM->getImagePath("nodetypes/Default/challenge.png");
+$CFG->issueicon = $HUB_FLM->getImagePath("nodetypes/Default/issue.png");
+$CFG->claimicon = $HUB_FLM->getImagePath("nodetypes/Default/claim.png");
+$CFG->solutionicon = $HUB_FLM->getImagePath("nodetypes/Default/solution.png");
+$CFG->evidenceicon = $HUB_FLM->getImagePath("nodetypes/Default/litertaure-alaysis.png");
+$CFG->resourceicon = $HUB_FLM->getImagePath("nodetypes/Default/reference-32x32.png");
+$CFG->orgicon = $HUB_FLM->getImagePath("nodetypes/Default/organization.png");
+$CFG->projecticon = $HUB_FLM->getImagePath("nodetypes/Default/project.png");
+$CFG->proicon = $HUB_FLM->getImagePath("nodetypes/Default/plus-32x32.png");
+$CFG->conicon = $HUB_FLM->getImagePath("nodetypes/Default/minus-32x32.png");
+$CFG->themeicon = $HUB_FLM->getImagePath("nodetypes/Default/theme.png");
 
-	/** EDIT THIS LINE **/
-	$CFG->maptilesurl = "<***url to your tilecache***>";
+/** EDIT THIS LINE 
+ * We ran our own Open Street map tile cache service, once Google maps went to payments
+ * So this should point to tilcache service of some sort.
+ **/
+$CFG->maptilesurl = "<***url to your tilecache***>";
 
 /** CREATE ALL OTHER GLOBAL VARIABLES AND INITIALIZE THEM AND LOAD LIBRARIES **/
 
-	require_once('core/sanitise.php');
- 	require_once("language/language.php");
- 	require_once("core/databases/sqlstatements.php");
-	require_once("core/datamodel/datamodel.class.php");
-    require_once('core/accesslib.php');
+require_once('core/sanitise.php');
+require_once("language/language.php");
+require_once("core/databases/sqlstatements.php");
+require_once("core/datamodel/datamodel.class.php");
+require_once('core/accesslib.php');
 
-    startSession();
+startSession();
 
-    unset($USER);
-	unset($HUB_DATAMODEL);
+unset($USER);
+unset($HUB_DATAMODEL);
 
-    global $CFG;
-    global $LNG;
-    global $USER;
-    global $HEADER;
-    global $BODY_ATT;
-    global $CONTEXT;
-    global $HUB_DATAMODEL;
-    global $HUB_SQL;
+global $CFG;
+global $LNG;
+global $USER;
+global $HEADER;
+global $BODY_ATT;
+global $CONTEXT;
+global $HUB_DATAMODEL;
+global $HUB_SQL;
 
-    $HEADER = array();
+$HEADER = array();
 
 /**CREATE THE DATAMODEL CLASS INSTANCE */
-	$HUB_DATAMODEL = new DataModel();
-	$HUB_DATAMODEL->load();
+$HUB_DATAMODEL = new DataModel();
+$HUB_DATAMODEL->load();
 
 /** SETUP THE DATABASE MANAGER **/
-	unset($DB);
-	require_once("core/databases/databasemanager.class.php");
-	// instantiate the database
-	if (isset($CFG->databasetype) && $CFG->databasetype != "") {
-		$DB = new DatabaseManager($CFG->databasetype);
-	} else {
-		$DB = new DatabaseManager();
-	}
-    global $DB;
+unset($DB);
+require_once("core/databases/databasemanager.class.php");
+// instantiate the database
+if (isset($CFG->databasetype) && $CFG->databasetype != "") {
+	$DB = new DatabaseManager($CFG->databasetype);
+} else {
+	$DB = new DatabaseManager();
+}
+global $DB;
 
-    require_once('core/datamodel/error.class.php');
-    require_once('core/apilib.php');
-    require_once('core/auditlib.php');
+require_once('core/datamodel/error.class.php');
+require_once('core/apilib.php');
+require_once('core/auditlib.php');
 
-    if (isset($_SESSION["session_userid"]) && $_SESSION["session_userid"] != "") {
-    	$USER = new User($_SESSION["session_userid"]);
-    	$USER->load();
-    } else {
-    	$USER = new User();
-    }
+if (isset($_SESSION["session_userid"]) && $_SESSION["session_userid"] != "") {
+	$USER = new User($_SESSION["session_userid"]);
+	$USER->load();
+} else {
+	$USER = new User();
+}
 
-    // load Themes from Database
-    // NOTE: all themes own by admin users so not duplicated
-    // If this changes then this code needs to call a function for get distinct by labels
-	$ns = getNodesByGlobal(0,-1,'name','ASC', 'Theme', '', 'long',"",'all',false);
-	$nodes = $ns->nodes;
+// load Themes from Database
+// NOTE: all themes own by admin users so not duplicated
+// If this changes then this code needs to call a function for get distinct by labels
+$ns = getNodesByGlobal(0,-1,'name','ASC', 'Theme', '', 'long',"",'all',false);
+$nodes = $ns->nodes;
 
-	$CFG->THEMES = array();
-	foreach($nodes as $node) {
-		array_push($CFG->THEMES, $node->name);
-	}
+$CFG->THEMES = array();
+foreach($nodes as $node) {
+	array_push($CFG->THEMES, $node->name);
+}
 ?>
